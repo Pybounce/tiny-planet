c755dd19ebfb5a0394dfedd04a535738193472ff
Pybounce <80575781+Pybounce@users.noreply.github.com>
2022-08-04 13:12:54 +0100

    Started GroundEnvironmentGenerator
    Cleaner code and structure.

--- Assets/Generators/GroundEnvironmentGenerator.cs
+++ Assets/Generators/GroundEnvironmentGenerator.cs
@@ -1,9 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using PybUtilities;
+
 
 [RequireComponent(typeof(PlanetEnvironmentGenerator))]
 public class GroundEnvironmentGenerator : MonoBehaviour
 {
+    private Vector3[] terrainVerts;
+    private GroundEnvironmentConfig[] groundConfigs;
+
+
+    /// <summary>
+    /// Loops through each vert and places an object if needed (based on groundEnvironmentConfig).
+    /// </summary>
+    public void Generate()
+    {
+        foreach (Vector3 _vert in terrainVerts)
+        {
+            foreach (GroundEnvironmentConfig _config in groundConfigs)
+            {
+
+            }
+        }
+    }
+
+    /// <summary>
+    /// Returns the environmental prefab containing the object to be placed.
+    /// </summary>
+    private bool ConfigPassBasicChance(GroundEnvironmentConfig _config)
+    {
+        return PybMath.Chance(_config.SpawnChance);
+    }
+
+    private GroundEnvironmentObject GetObjectToPlace(GroundEnvironmentConfig _config)
+    {
+        int _randomObjChance = Random.Range(0, _config.TotalWeight);
+        foreach (var _prefab in _config.EnvironmentPrefabs)
+        {
+            if (_prefab.SpawnWeight <= _randomObjChance) { return _prefab; }
+            _randomObjChance -= _prefab.SpawnWeight;
+        }
+        if (_config.EnvironmentPrefabs == null || _config.EnvironmentPrefabs.Length <= 0)
+        {
+            throw new System.Exception("Config must contain at least 1 envionmental config.");
+        }
+        Debug.LogWarning("GroundEnvironmentGenerator.GetObjectToPlace - chance check broken, returning default first.");
+        return _config.EnvironmentPrefabs[0];   //If for some reason the above chance check doesn't pass, it will default to returning the first object.
+
+    }
+
 
 }
--- Assets/Generators/PlanetGenerator.cs
+++ Assets/Generators/PlanetGenerator.cs
@@ -5,62 +5,13 @@ using PybUtilities;
 
 public class PlanetGenerator : MonoBehaviour
 {
-    private Vector3[] verts;
-    private PlanetEnvironmentConfig config;
+    private PlanetTerrainGenerator planetTerrainGenerator;
+    private PlanetEnvironmentGenerator planetEnvironmentGenerator;
 
-    public PlanetGenerator(PlanetEnvironmentConfig _config, Vector3[] _verts)
+    private void Awake()
     {
-        this.verts = _verts;
-        this.config = _config;
-    }
+        planetTerrainGenerator = this.gameObject.AddComponent<PlanetTerrainGenerator>();
+        planetEnvironmentGenerator = this.gameObject.AddComponent<PlanetEnvironmentGenerator>();
 
-    public void GeneratePlanet()
-    {
-        if (CanGenerate() == false) { return; }
-
-    }
-
-    private void GenerateGroundObjects(GroundEnvironmentConfig[] _configs)
-    {
-        foreach (Vector3 _vert in verts)
-        {
-            foreach (GroundEnvironmentConfig _config in _configs)
-            {
-                //do normal chance
-                //if pass, do perlin chance
-                //if pass get object
-                //if any fail, continue
-                if (_config.EnvironmentPrefabs.Length <= 0) { continue; }   //No objects in config to place
-                if (PybMath.Chance(_config.SpawnChance) == false) { continue; } //Failed spawn chance check
-                if (false) { continue; }    //*** ***Perlin Chance Check Goes Here*** ***\\  //Failed perlin chance check
-                int _totalWeight = 0;
-                foreach (var _prefab in _config.EnvironmentPrefabs)
-                {
-                    _totalWeight += _prefab.SpawnWeight;
-                }
-                GroundEnvironmentObject _selectedPrefab;
-                int _randomObjChance = Random.Range(0, _totalWeight);
-                foreach (var _prefab in _config.EnvironmentPrefabs)
-                {
-                    _selectedPrefab = _prefab;
-                    if (_prefab.SpawnWeight <= _randomObjChance) { break; }
-                    _randomObjChance -= _prefab.SpawnWeight;
-                }
-
-                //*** ***Spawn Object Here*** ***\\
-
-                break;  //If an object has been placed on this point, we don't need to check the other configs.
-            }
-        }
-    }
-
-    /// <summary>
-    /// Checks the input data
-    /// </summary>
-    private bool CanGenerate()
-    {
-        if (verts == null || verts.Length <= 0) { return false; }
-        if (config == null) { return false; }
-        return true;
     }
 }
